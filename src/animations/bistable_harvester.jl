f671f9f315480c72d572987dd8462cf0c51c80a7
csaintmartin <camille.saint-martin@univ-smb.fr>
2023-07-06 16:56:03 +0200

    added update of bistable harvester

--- src/animations/bistable_harvester.jl
+++ src/animations/bistable_harvester.jl
@@ -1,9 +1,24 @@
 using InteractiveDynamics
 using DynamicalSystems, GLMakie, OrdinaryDiffEq
+using LaTeXStrings
 
 function bistable_trajectory()
-    xw = 0.5e-3
-    omega0 = 1
+    function potential_energy(x, p0)
+        xw = p0[1]
+        omega0 = p0[2]
+        M = p0[9]
+        return (M * omega0^2 / (8 * xw^2)) * (x + xw)^2 * (x - xw)^2
+    end
+
+    function potential_energy_state(u)
+        x = u[1]
+        xw = p0[1]
+        omega0 = p0[2]
+        M = p0[9]
+        return (M * omega0^2 / (8 * xw^2)) * (x + xw)^2 * (x - xw)^2
+    end
+    xw = 1.0
+    omega0 = 1.0
     Q = 87.0
     fd = 50.0
     omegad = 2.0 * pi * fd
@@ -11,17 +26,16 @@ function bistable_trajectory()
     alpha = 0.068
     C0 = 1.05e-6
     R = 7.83e3
-    # K_harvesting_APA = 0.3e6
     M = 17.3e-3
     L=25e-3
-    global x = 0
 
     function bistable_harvester!(du, u, p, t)
         xw, omega0, Q, omegad, Ad, alpha, C0, R, M, L = p
-        # L = (xw / omega0) * sqrt(4 * K_harvesting_APA / M)
+
         x = u[1]
         dotx = u[2]
         v = u[3]
+
         du[1] = dotx
         du[2] = -(omega0^2 / 2.0) * (x^2 / xw^2 - 1.0) * x
             -omega0 / Q * dotx
@@ -32,7 +46,6 @@ function bistable_trajectory()
         return nothing
     end
 
-
     p0 = [xw, omega0, Q, omegad, Ad, alpha, C0, R, M, L]
     u0 = [xw, 1.0e-3, 0.0]
 
@@ -40,12 +53,12 @@ function bistable_trajectory()
         1 => 0.7*p0[1]:0.1e-3:3.0*p0[1],
         2 => 0:0.1e-3:2, 
         3 => 50:0.1e-3:150,
-        4 => 100:0.1:500
+        4 => 1.0:0.1:20.0
     )
-    pnames = Dict(1 => "xw", 
-        2 => "omega0", 
-        3 => "Q",
-        4 => "omegad"
+    pnames = Dict(1 => L"x_w",
+        2 => L"\omega_0",
+        3 => L"Q",
+        4 => L"\omega_d"
     )
 
     # limite du plan 3D (x, dotx, v)
@@ -57,30 +70,28 @@ function bistable_trajectory()
 
     ds = ContinuousDynamicalSystem(bistable_harvester!, u0, p0)
 
-
     N = 2
     u0s = [[xw, x / N / 2, 1.0] for x = 0:N-1]
 
     idxs = [1, 2]
-    diffeq = (alg=Tsit5(), dt=0.01, adaptive=true)
-
 
     figure, obs, step, paramvals = interactive_evolution(
-        ds, u0s; ps, idxs, tail=1000, pnames
+        ds, u0s; ps, idxs, tail=1000, tsidxs=nothing, pnames
     )
 
-    ax = Axis(figure[1,1][1,2]; xlabel = "Position", ylabel = "Énergie potentielle")
-    function distance_from_symmetry(u)
-        Ec = (M*omega0^2 / (8*xw^2)) * (x+xw)^2 * (x-xw)^2
-        return Ec
-    end
-    for (i, ob) in enumerate(obs)
-        ord = lift(abs -> distance_from_symmetry.(abs).*i/i, ob)
-        abs = 1:length(ord[])
+    #PLOT EQUILIBRIUM POSITIONS
+    bistable_equilibriums(xw) = [Point2f(-xw, 0.0), Point2f(xw, 0.0)]
+    equilibrium_observables = lift(bistable_equilibriums, paramvals[1])
+    ax_phase_plane = content(figure[1, 1][1, 1])
+    scatter!(ax_phase_plane, equilibrium_observables; markersize=16, marker=:diamond, color=:black)
 
-        lines!(ax, abs, ord; color = JULIADYNAMICS_COLORS[i])
+    ax = Axis(figure[1,1][1,2]; xlabel = L"\text{Position}, x", ylabel = L"\text{Énergie potentielle}, E_p [J]")
+
+    for (i, ob) in enumerate(obs)
+        y = lift(x -> potential_energy_state.(x), ob)
+        x_ = 1:length(y[])
+        scatter!(ax, x_, y; color=JULIADYNAMICS_COLORS[i])
     end
-    ax.limits = ((0, 1000), (0, 0.001))
-    figure
+    ax.limits = ((0, length(y[])), (minimum(y[]), maximum(y[])*1e-1))
 end
 
