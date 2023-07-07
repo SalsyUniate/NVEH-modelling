2981ab4d245a35cab5e16fcee6fa624b385fd608
SalsyUniate <cloe.leglise@free.fr>
2023-07-07 11:56:31 +0200

    add of harmonic oscillator to main window

--- src/window.jl
+++ src/window.jl
@@ -5,7 +5,7 @@ using LaTeXStrings
 include("animations/bistable_harvester.jl")
 include("animations/duffing_oscillator.jl")
 include("animations/linear_harvester.jl")
-
+include("animations/harmonic_oscillator.jl")
 
 const io = PipeBuffer()
 
@@ -18,7 +18,7 @@ grid = GtkGrid()
 b_bistable = GtkButton("Bistable harvester")
 b_linear = GtkButton("Linear harvester")
 b_duffing = GtkButton("Duffing oscillator")
-
+b_harmonic = GtkButton("Harmonic oscillator")
 
 signal_connect(b_bistable, "clicked") do widget, others...
     bistable_trajectory()
@@ -32,12 +32,15 @@ signal_connect(b_linear, "clicked") do widget, others...
     linear_trajectory()
 end
 
+signal_connect(b_harmonic, "clicked") do widget, others...
+    harmonic_trajectory()
+end
 
 # grid[1,1] = l_bistable
 grid[1,1] = b_bistable
 grid[1,2] = b_linear
 grid[1,3] = b_duffing
-
+grid[1,4] = b_harmonic
 
 push!(vbox, grid)
 set_gtk_property!(vbox, :expand, true)
